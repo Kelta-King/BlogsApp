526705c6cffb8e101e3378bbcdad37224aff7cb4
Kushang Shah <kushangshah131@gmail.com>
2021-01-07 23:55:23 +0530

    practice 1

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', views.about),
+    path('', views.home)
 ]
