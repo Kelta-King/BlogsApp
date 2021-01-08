f6d57c4679f1293814179e1f628c1074369a9495
Kushang Shah <kushangshah131@gmail.com>
2021-01-08 12:21:28 +0530

    models

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    #add thumbnail later
+    #add auther later
+
+    def __str__(self):
+        return self.title
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def articles_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
 
 def hey(request):
     return render(request, 'articles/hey.html')
