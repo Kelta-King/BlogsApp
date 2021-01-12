b2010187a48ca93b4fce7fa2acee628bd5efb30f
Kushang Shah <kushangshah131@gmail.com>
2021-01-12 08:30:29 +0530

    media upload

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
-    #add thumbnail later
+    thumb = models.ImageField('default.png', blank = True)
     #add auther later
 
     def __str__(self):
