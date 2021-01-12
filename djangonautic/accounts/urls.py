a05862b3bd552a857140ba5db758b9d06df7d4ec
Kushang Shah <kushangshah131@gmail.com>
2021-01-12 10:39:44 +0530

    user

--- djangonautic/accounts/urls.py
+++ djangonautic/accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup', views.hey, name='signup'),
+    path('signup', views.signup, name='signup'),
 ]
--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
-def hey(request):
+def signup(request):
     
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #login user
+
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    
     return render(request, 'accounts/signup.html', {"form":form })
