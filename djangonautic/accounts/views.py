106fc21cfa40c8b7b4c300acc8d54d74d92ed8fa
Kushang Shah <kushangshah131@gmail.com>
2021-01-12 19:43:41 +0530

    login

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 # Create your views here.
 
@@ -16,3 +16,17 @@ def signup(request):
         form = UserCreationForm()
     
     return render(request, 'accounts/signup.html', {"form":form })
+
+def login(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+
+        if form.is_valid():
+            #login user
+            return redirect('articles:list')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', { 'form':form })
